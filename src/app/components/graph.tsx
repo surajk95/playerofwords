9c841a6fbfa33092699bd4045a554dda1a27d0e5
Suraj <surajkukati95@gmail.com>
2024-05-29 15:05:34 +0530

    added sparkline

--- src/app/components/graph.tsx
+++ src/app/components/graph.tsx
@@ -1,5 +1,23 @@
+'use client'
+import { useEffect, useState } from "react"
+import { useScoreStore } from "./store"
+import { Sparklines, SparklinesLine, SparklinesSpots } from 'react-sparklines';
+
 export default function Graph() {
+    const speed = useScoreStore((state) => (state.speed))
+    const [speedList, setSpeedList] = useState<number[]>([]);
+
+    useEffect(() => {
+        setSpeedList((speedList) => [...speedList, speed]);
+    }, [speed])
+
     return (
-        <div>Graph</div>
+        <div style={{ width: '100%', height: '200px' }}>
+            Graph
+            <Sparklines data={speedList} limit={10}>
+                <SparklinesLine style={{ fill: "none" }} />
+                <SparklinesSpots />
+            </Sparklines>
+        </div>
     )
 }
