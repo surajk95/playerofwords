3b1a58dafea56817b65a358b436a40e0254e3083
Suraj <surajkukati95@gmail.com>
2024-06-01 00:05:34 +0530

    added attribution

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,6 +12,7 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <h1 className={`${styles.appTitle} ${secondaryFont.className}`}>Player<br />of<br />words</h1>
+      <a className={`${styles.attribution} ${secondaryFont.className}`} href="https://github.com/surajk95/playerofwords">github</a>
       <div className={styles.appContainer}>
         <div className={styles.mainContent}>
           <Loader />
